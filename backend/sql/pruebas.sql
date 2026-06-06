2413b55c7762cbbfbb88e4ee752ed95585614cfe
Julio tzicap <juliotzicap@umes.edu.gt>
2026-06-05 18:23:04 -0600

    Actualizar pruebas SQL

--- backend/sql/pruebas.sql
+++ backend/sql/pruebas.sql
@@ -7,3 +7,36 @@ SELECT * FROM vista_ocupacion_mensual;
 SELECT * FROM vista_top_huespedes;
 
 SELECT obtener_saldo_reserva(4);
+
+-- después de ejecutar el procedimiento se puede ejecutar esta prueba
+CALL realizar_reserva(
+    1,
+    2,
+    '2026-07-01',
+    '2026-07-05',
+    2
+);
+
+SELECT * FROM reservas
+ORDER BY id_reserva DESC;
+
+-- probar error de solape
+CALL realizar_reserva(
+    2,
+    2,
+    '2026-07-03',
+    '2026-07-06',
+    2
+);
+
+-- probar chekout, la reserva 4 debe estar "en curso"
+CALL realizar_checkout(
+    4,
+    2225,
+    'Tarjeta'
+);
+
+-- despues de ejecutar eso debe estar "completada"
+SELECT * FROM reservas
+WHERE id_reserva = 4;
+
